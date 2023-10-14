1b8f2d83435c67d1b50bebd6b9ad51ae2beb31cd
pearses <scott.pearse@dewc.com>
2023-10-14 11:52:48 +1030

    feat: basic CRUD functionality working

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from models import ToDo  # Correct the import statement
+from .models import ToDo  # Correct the import statement
 
 app = FastAPI()
 
 todolist = []  # Rename to `todolist`
+
+# Post
+@app.post("/todolist")  # Use @app.post to create a to-do
+async def post_todo(todo: ToDo):  # Use the correct variable name 'todo'
+    todolist.append(todo)  # Correct the variable name 'todolist'
+    return {"message": "Todo has been added!"}
 
 # Get All
 @app.get("/todolist")
@@ -12,15 +18,28 @@ async def get_all_todos():
 
 # Get Single ID
 @app.get("/todolist/{todo_id}")
-async def read_item(todo_id):
-    return {"id": todo_id}
-
-# Post
-@app.post("/todolist")  # Use @app.post to create a to-do
-async def post_todo(todo: ToDo):  # Use the correct variable name 'todo'
-    todolist.append(todo)  # Correct the variable name 'todolist'
-    return {"message": "Todo has been added!"}
+async def get_todo(todo_id: int):
+    for todo in todolist:
+        if todo.id == todo_id:
+            return {"todo": todo}
+    return {"message":"todo notfound !"}
 
 # Put
+@app.put("/todolist/{todo_id}")
+async def update_todo(todo_id: int, todo_obj: ToDo):
+    for todo in todolist:
+        if todo.id == todo_id:
+            todo.id = todo_id
+            todo.item = todo_obj.item
+            todolist.append(todo)
+            return {"message":"todo has been updated !"}
+    return {"message":"No todos found !"}
 
 # Delete
+@app.delete("/todolist/{todo_id}")
+async def delete_todo(todo_id: int):
+    for todo in todolist:
+        if todo.id == todo_id:
+            todolist.remove(todo)
+            return {"message":"todo has been deleted !"}
+    return {"message":"todo not found !"}  
